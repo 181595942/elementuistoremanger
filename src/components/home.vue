cd3df237ed7a974f9aebef7d2d7b881db0946d7d
181595942 <181595942@qq.com>
2021-01-12 12:25:03 +0800

    login已完成

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <el-button type ="info"  @click="exit"> 退出 </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'home',
+    data() {
+        return {
+        
+        }
+    },
+    methods: {
+        exit(){
+            window.sessionStorage.clear()
+            this.$router.push('/login')
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
